905dfa91e568dca8fd4f0d1eb10b9d32182566c5
DuctopwH <webstor.38@gmail.com>
2021-12-12 01:14:37 +0300

    Working on GUI

--- main_gui.py
+++ main_gui.py
@@ -1,52 +1,65 @@
 import tkinter as tk
-from random import randrange
 from PIL import Image, ImageTk
 from tkinter.constants import LEFT, N, W
 
-root = tk.Tk()
-root.title('GuessTheNumber')
-root.geometry('500x490')
-root.resizable(width=False, height=False)
 
-image_file = Image.open('source/gtn_logo.PNG')
+def new_play_window():
+    level_window.destroy()
+    play_window = tk.Toplevel(root)
+    play_window.title('GuessTheNumber')
+    play_window.geometry('500x500')
+    play_window.resizable(width=False, height=False)
+
+
+root = tk.Tk()
+root.withdraw()
+
+level_window = tk.Tk()
+level_window.iconbitmap('source/gtn_icon.ico')
+level_window.title('GuessTheNumber')
+level_window.geometry('500x520')
+level_window.resizable(width=False, height=False)
+
+image_file = Image.open('source/gtn_logo.png')
 resized_image = image_file.resize((472, 130))
 image_logo = ImageTk.PhotoImage(resized_image)
 
 lbl_logo = tk.Label(image=image_logo)
-lbl_htp = tk.Label(root,
-                        text='How to Play',
-                        font='Arial 20')
-lbl_htp_text = tk.Label(root,
+lbl_htp = tk.Label(level_window,
+                   text='How to Play',
+                   font='Arial 20')
+lbl_htp_text = tk.Label(level_window,
                         text='The program will pick a secret number.\n'
-                        'You guess what number is it.',
+                             'You guess what number is it.\n'
+                             '\nPick a difficulty level and press "Start"',
                         font='Arial 14',
                         justify=LEFT)
-framelvl = tk.Frame(root)
+framelvl = tk.Frame(level_window)
 
 lbl_lvleasy = tk.Label(framelvl,
-                        text='Easy',
-                        font='Arial 15',
-                        foreground='#004d00',
-                        width=15)
+                       text='Easy',
+                       font='Arial 15',
+                       foreground='#004d00',
+                       width=15)
 lbl_lvleasy_text = tk.Label(framelvl,
-                        text='(1 to 10)',
-                        font='Arial 12')
+                            text='(1 to 10)',
+                            font='Arial 10')
 lbl_lvlnormal = tk.Label(framelvl,
-                        text='Normal',
-                        font='Arial 15',
-                        foreground='#f48f00',
-                        width=15)
+                         text='Normal',
+                         font='Arial 15',
+                         foreground='#f48f00',
+                         width=15)
 lbl_lvlnormal_text = tk.Label(framelvl,
-                        text='(1 to 100)',
-                        font='Arial 12')                        
+                              text='(1 to 100)',
+                              font='Arial 10')
 lbl_lvlhard = tk.Label(framelvl,
-                        text='Hard',
-                        font='Arial 15',
-                        foreground='#7b000f',
-                        width=15)
+                       text='Hard',
+                       font='Arial 15',
+                       foreground='#7b000f',
+                       width=15)
 lbl_lvlhard_text = tk.Label(framelvl,
-                        text='(-100 to 100)',
-                        font='Arial 12')
+                            text='(-100 to 100)',
+                            font='Arial 10')
 
 picked_level = tk.IntVar()
 picked_level.set(0)
@@ -54,15 +67,16 @@ rad_easy = tk.Radiobutton(framelvl, variable=picked_level, value=0)
 rad_normal = tk.Radiobutton(framelvl, variable=picked_level, value=1)
 rad_hard = tk.Radiobutton(framelvl, variable=picked_level, value=2)
 btn_start = tk.Button(framelvl,
-                        text='Start game',
-                        font='Arial 20',
-                        width=10, height=1)
+                      text='Start game',
+                      font='Arial 20',
+                      width=10, height=1,
+                      command=new_play_window)
 
 lbl_logo.grid(row=0, column=0, sticky=N, columnspan=3)
 lbl_htp.grid(row=1, column=0, sticky=N, columnspan=3, pady=(20, 0))
 lbl_htp_text.grid(row=2, column=0, sticky=N, columnspan=3, pady=(0, 30))
 framelvl.grid(row=3, column=0, columnspan=3)
-btn_start.grid(row=4, column=0, columnspan=3, pady=(30))
+btn_start.grid(row=4, column=0, columnspan=3, pady=30)
 
 lbl_lvleasy.grid(row=0, column=0)
 lbl_lvlnormal.grid(row=0, column=1)
@@ -74,4 +88,4 @@ rad_easy.grid(row=2, column=0)
 rad_normal.grid(row=2, column=1)
 rad_hard.grid(row=2, column=2)
 
-root.mainloop()
+level_window.mainloop()
